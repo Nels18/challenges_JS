b01bacc34b81008f6fe9b48c2b030c944d94c362
Nelson Belgarde <nelsonbelgarde@gmail.com>
2021-12-02 18:02:22 +0100

    Football app : finish exercise 3

--- challenge_13/script.js
+++ challenge_13/script.js
@@ -5,39 +5,39 @@
 Supposons que nous récupérons les données d’un service web à propos d’un certain jeu (variable ‘game’ dans le code en dessous). Dans ce challenge, on va travailler avec ces données. */
 
 const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
   players: [
     [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnabry',
-      'Lewandowski',
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnabry",
+      "Lewandowski",
     ],
     [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
     ],
   ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnabry', 'Lewandowski','Hummels'],
-  date: 'Nov 9th, 2037',
+  score: "4:0",
+  scored: ["Lewandowski", "Gnabry", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
   odds: {
     team1: 1.33,
     x: 3.25,
@@ -45,19 +45,21 @@ const game = {
   },
 };
 
-
-// 🏁 Exercice 1
+console.log('******** 🏁 Exercice 1 ********');
 
 // - Créer un tableau de joueurs pour chaque équipe (variables ‘players1’ et ‘players2’).
 const players1 = game.players[0];
 const players2 = game.players[1];
+
+console.log("players1 :", players1);
+console.log("players2 :", players2);
 
 // - Le premier joueur dans un tableau de joueurs est le gardien et les autres sont des joueurs de terrain. Pour l’équipe 1 (le Bayern de Munich), créer une variable ‘gk’ avec le nom du gardien et un tableau ‘fieldPlayers’ avec le reste des joueurs de l’équipe.
 
 let gk;
 let fieldPlayers = [];
 
-players1.forEach(player => {
+players1.forEach((player) => {
   if (0 == players1.indexOf(player)) {
     gk = player;
   } else {
@@ -65,19 +67,30 @@ players1.forEach(player => {
   }
 });
 
+console.log("gk :", gk);
+console.log("fieldPlayers :", fieldPlayers);
+
 // - Créer un tableau ‘allPlayers’ qui contient les joueurs de chaque équipe (22 joueurs).
 
-const allPlayers = [...players1, ...players2]
+const allPlayers = [...players1, ...players2];
+
+console.log("allPlayers :", allPlayers);
 
 // - Pendant le match, le Bayern utilise 3 joueurs remplaçants. Créer un autre tableau ‘players1Final’ contenant tous les joueurs de l’équipe originelle ainsi que ‘Thiago’ ‘Coutinho’ et ‘Perisic’.
 
-const players1Final = [...players1,...["Thiago", "Coutinho", "Perisic"]];
+const players1Final = [...players1, ...["Thiago", "Coutinho", "Perisic"]];
+
+console.log("players1Final :", players1Final);
 
 // - Basé sur l’objet game.odds, créer une variable pour chaque probabilité (appelés ‘team1’, ‘draw’ et ‘team2’).
 
-const team1 = game.odds.team1
-const draw = game.odds.x
-const team2 = game.odds.team2
+const team1 = game.odds.team1;
+const draw = game.odds.x;
+const team2 = game.odds.team2;
+
+console.log("team1 :", team1);
+console.log("draw :", draw);
+console.log("team2 :", team2);
 
 // - Écrire une fonction ‘printGoals’ qui prends un nombre arbitraire de joueurs et qui écrit chacun d’eux dans la console , ainsi que le nombre total de buts.
 // (On suppossant que l'on doit afficher le nombre total de buts depuis le début de la saison)
@@ -90,11 +103,14 @@ const printGoals = (numberOfPlayer) => {
     if (!players.includes(player)) {
       players.push(player);
 
-      console.log(`${player} : ${(Math.floor(Math.random() * 60 ) + 1)} but(s) au total.`);
+      console.log(
+        `${player} : ${Math.floor(Math.random() * 60) + 1} but(s) au total.`
+      );
     }
   }
-}
+};
 
+printGoals(8);
 
 // - L’équipe avec la probabilité la plus basse est celle la plus susceptible de gagner. Écrire dans la console quelle équipe a le plus de chance de gagner.
 const printFavory = () => {
@@ -106,22 +122,12 @@ const printFavory = () => {
   }
 
   console.log(message);
-}
+};
 
-// console.log('players1 :', players1);
-// console.log('players2 :', players2);
-// console.log('gk :', gk);
-// console.log('fieldPlayers :', fieldPlayers);
-// console.log('allPlayers :', allPlayers);
-// console.log('players1Final :', players1Final);
-// console.log('team1 :', team1);
-// console.log('draw :', draw);
-// console.log('team2 :', team2);
-// printGoals(8);
-// printFavory();
+printFavory();
 
-
-// 🏁 Exercice 2
+console.log(' ');
+console.log('******** 🏁 Exercice 2 ********');
 
 // Faire une boucle sur le tableau game.scored et afficher dans la console chaque nom de butteur ainsi que le numéro du but : (Exemple : “But 1 : Lewanoski”).
 
@@ -130,27 +136,29 @@ const printScorers = () => {
     const scorer = game.scored[index];
     console.log(`But ${index + 1} : ${scorer}.`);
   }
-}
+};
 
+printScorers();
 
 // Faire une boucle qui calcule la moyenne des probabilités et l’écrire dans la console.
 const printAverageOdds = () => {
   let sumOdds = 0;
   let nbOdds = 0;
-  
+
   for (const key in game.odds) {
     if (Object.hasOwnProperty.call(game.odds, key)) {
       const odd = game.odds[key];
-      nbOdds++
-      sumOdds  += odd;
+      nbOdds++;
+      sumOdds += odd;
     }
   }
-  
-  const averageOdds = sumOdds / nbOdds;
-  
-  console.log('Moyenne des probabilités :', averageOdds.toFixed(2));
-}
 
+  const averageOdds = sumOdds / nbOdds;
+
+  console.log("Moyenne des probabilités :", averageOdds.toFixed(2));
+};
+
+printAverageOdds();
 
 /* Afficher les 2 probabilités dans la console, mais dans un joli format comme ceci :
 Probabilité de victoire pour Bayern Munich : 1,33
@@ -162,15 +170,16 @@ const printOdds = () => {
   for (const key in game.odds) {
     if (Object.hasOwnProperty.call(game.odds, key)) {
       const odd = game.odds[key];
-      if ('x' == key) {
+      if ("x" == key) {
         console.log(`Probabilité d’égalité : ${odd}`);
       } else {
         console.log(`Probabilité de victoire pour ${game[key]} : ${odd}`);
       }
     }
   }
-}
+};
 
+printOdds();
 
 // Créer un objet appelé ‘scorers’ qui contient le nom des joueurs qui ont marqué en propriété et le nombre de buts qu’ils ont marqué en value. Exemple :
 /* {
@@ -190,9 +199,83 @@ for (let index = 0; index < game.scored.length; index++) {
   }
 }
 
+console.log("scorers :", scorers);
 
-printScorers();
-printAverageOdds();
-printOdds();
-console.log('scorers :', scorers);
+console.log(' ');
+console.log('******** 🏁 Exercice 3 ********');
 
+/* Cette fois-ci, nous avons un tableau à 2 dimensions appelé ‘gameEvents’ (voir en dessous) avec le log de tous les évènements qui se sont passés pendant le match. La première case d’un sous-tableau correspond à la minute de l’évènement, la 2ème case correspond au type d’évènement.
+ */
+
+// Données :
+const gameEvents = [
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🟨 Yellow card"],
+  [69, "🟥 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🟨 Yellow card"],
+];
+
+// Créer un tableau ‘events’ des différents évènements qui se sont déroulés (pas de doublon !).
+let events = [];
+const getEvents = () => {
+  gameEvents.forEach((event) => {
+    if (!events.includes(event[1])) {
+      events.push(event[1]);
+    }
+  });
+};
+
+getEvents();
+console.log("gameEvents :", gameEvents);
+
+// Après que le jeux soit fini, on a décidé que le carton jaune de la minute 64 n’était pas juste, Le supprimer du log d’évènements.
+
+const cancelEvent = (minute, eventName) => {
+  for (let index = 0; index < gameEvents.length; index++) {
+    const event = gameEvents[index];
+
+    if (event[0] == minute && event[1] == eventName) {
+      gameEvents.splice(index, 1);
+    }
+  }
+};
+cancelEvent(64, "🟨 Yellow card");
+console.log("gameEvents :", gameEvents);
+
+// Calculer et afficher ce texte dans la console : “Un évènement est apparu en moyenne toutes les 9 minutes”. (Un jeu de foot dure 90 minutes).
+
+const durationGame = 90;
+const printFrequencyOfEvents = () => {
+  const nbEvents = gameEvents.length;
+  console.log(
+    `Un évènement est apparu en moyenne toutes les ${
+      durationGame / nbEvents
+    } minutes.`
+  );
+};
+
+printFrequencyOfEvents();
+
+/* Faire une boucle sur ‘gameEvents’ et afficher pour chaque élément dans la console s’il s’est déroulé dans la première ou deuxième moitié du jeu :
+[PREMIÈRE MOITIÉ] 17 . ⚽️ GOAL */
+
+const printHalfTime = () => {
+  let halfTime;
+  gameEvents.forEach((event) => {
+    if (45 > event[0]) {
+      halfTime = "[PREMIÈRE MOITIÉ]";
+    } else {
+      halfTime = "[SECONDE MOITIÉ]";
+    }
+    console.log(`${halfTime} ${event[0]} . ${event[1]}`);
+  });
+};
+
+printHalfTime();
